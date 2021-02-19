d36b595d43b8febb35019a8132cf1b5fd8914475
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2021-02-19 14:46:43 +0530

    ♨ Helper Function Improved

--- src/components/SpaceForceNew.jsx
+++ src/components/SpaceForceNew.jsx
@@ -1,5 +1,7 @@
+// Next goal is to make things modular and make use of the helper function
 import React, { useEffect, createRef } from "react";
 import songFile from "../audio/water.mp3";
+import { getPoints } from "../utils/helper";
 
 // Changing Variables
 let ctx, x_end, y_end, bar_height, rafId;
@@ -7,9 +9,11 @@ let ctx, x_end, y_end, bar_height, rafId;
 // constants
 const width = window.innerWidth;
 const height = window.innerHeight;
-const bars = 100;
-const bar_width = 1;
-const radius = 75;
+const bars = 64;
+const radius = 50;
+const bar_width = 15;
+const fftSize = 1024;
+
 const center_x = width / 2;
 const center_y = height / 2;
 
@@ -18,6 +22,8 @@ const NewSpaceForce = () => {
   const context = new (window.AudioContext || window.webkitAudioContext)();
   const canvas = createRef();
   const analyser = context.createAnalyser();
+  // 1024 seems the best yet for fftSize
+  analyser.fftSize = fftSize;
   const frequency_array = new Uint8Array(analyser.frequencyBinCount);
   useEffect(() => {
     console.log("component will mount executed");
@@ -26,69 +32,82 @@ const NewSpaceForce = () => {
     source.connect(analyser);
     analyser.connect(context.destination);
   }, []);
+  // let points = getPoints(
+  //   "circle",
+  //   height / 2,
+  //   [width / 2, height / 2],
+  //   frequency_array.length,
+  //   frequency_array,
+  //   { offset: 35, rotate: 270 }
+  // );
+
+  // Shape => circle
+  // size => height/2 => the viz will take half the screen
+  // originX, originY => [width / 2, height / 2], => The origin of the circle would be in the middle of the screen
+  // pointCount => data.length, => Point count would be the number of elements in our data array
+  // endPoints => data
+  // options ={} => { offset: 35, rotate: 270 }
 
   const animationLooper = (canvas) => {
     canvas.width = width;
     canvas.height = height;
 
     ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // This makes the background black
+    // ctx.fillStyle = "black";
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // This makes the inner circle (is not visible on black background)
+
     ctx.beginPath();
     ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
     ctx.stroke();
+
     analyser.getByteFrequencyData(frequency_array);
-    ctx.font = "500 24px Helvetica Neue";
+    // The average value here is just used to make the color of the bars and text react to the music
     const avg =
       [...Array(255).keys()].reduce(
         (acc, curr) => acc + frequency_array[curr],
         0
       ) / 255;
-    ctx.fillStyle = "rgb(" + 200 + ", " + (200 - avg) + ", " + avg + ")";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "top";
-    ctx.fillText("SPACE", canvas.width / 2, canvas.height / 2 - 24);
-    ctx.fillText("FORCE", canvas.width / 2, canvas.height / 2 + 6);
+
+    // This is text styling
+    // ctx.font = "500 24px Helvetica Neue";
+    // ctx.fillStyle = "rgb(" + 50 + ", " + (200 - avg) + ", " + avg + ")";
+    // ctx.textAlign = "center";
+    // ctx.textBaseline = "top";
+    // ctx.fillText("SPACE", canvas.width / 2, canvas.height / 2 - 24);
+    // ctx.fillText("FORCE", canvas.width / 2, canvas.height / 2 + 6);
 
     for (var i = 0; i < bars; i++) {
       let radians = (Math.PI * 2) / bars;
-      let bar_height = frequency_array[i] * 0.5;
+      // this defines the height of the bar
+      let bar_height = frequency_array[i] * 1;
 
+      // x and y are coordinates of where the end point of a bar any second should be
       let x = canvas.width / 2 + Math.cos(radians * i) * radius;
       let y = canvas.height / 2 + Math.sin(radians * i) * radius;
       let x_end =
         canvas.width / 2 + Math.cos(radians * i) * (radius + bar_height);
       let y_end =
         canvas.height / 2 + Math.sin(radians * i) * (radius + bar_height);
-      let color =
-        "rgb(" +
-        200 +
-        ", " +
-        (200 - frequency_array[i]) +
-        ", " +
-        frequency_array[i] +
-        ")";
+      // let color =
+      //   "rgb(" +
+      //   200 +
+      //   ", " +
+      //   (200 - frequency_array[i]) +
+      //   ", " +
+      //   frequency_array[i] +
+      //   ")";
+      let color = "#FF69B4";
       ctx.strokeStyle = color;
-      ctx.lineWidth = 3;
+      ctx.lineWidth = bar_width;
       ctx.beginPath();
       ctx.moveTo(x, y);
       ctx.lineTo(x_end, y_end);
       ctx.stroke();
     }
   };
-
-  // function draw() {
-
-  //   // Draw Background
-
-  //   // Draw circle
-
-  //   // Draw label
-
-  //   // Draw bars
-
-  //   requestAnimationFrame(draw);
-  // }
 
   const togglePlay = () => {
     if (audio.paused) {
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -8,7 +8,7 @@ export const __rotatePoint__ = (
   degree
 ) => {
   //clockwise
-  let angle = this.__toRadians__(degree);
+  let angle = __toRadians__(degree);
   let rotatedX =
     Math.cos(angle) * (pointX - originX) -
     Math.sin(angle) * (pointY - originY) +
@@ -128,7 +128,7 @@ export const getPoints = (
 
   if (shape === "circle") {
     let degreePerPoint = 360 / pointCount;
-    let radianPerPoint = this.__toRadians__(degreePerPoint);
+    let radianPerPoint = __toRadians__(degreePerPoint);
     let radius = size / 2;
 
     for (let i = 1; i <= pointCount; i++) {
@@ -138,7 +138,7 @@ export const getPoints = (
 
       let x = originX + (radius - pointOffset) * Math.cos(currentRadian);
       let y = originY + (radius - pointOffset) * Math.sin(currentRadian);
-      let point1 = this.__rotatePoint__([x, y], [originX, originY], rotate);
+      let point1 = __rotatePoint__([x, y], [originX, originY], rotate);
 
       rtn.start.push(point1);
 
@@ -148,7 +148,7 @@ export const getPoints = (
       y =
         originY +
         (radius - pointOffset + currentEndPoint) * Math.sin(currentRadian);
-      let point2 = this.__rotatePoint__([x, y], [originX, originY], rotate);
+      let point2 = __rotatePoint__([x, y], [originX, originY], rotate);
 
       rtn.end.push(point2);
     }
@@ -166,14 +166,14 @@ export const getPoints = (
       let degree = rotate;
       let pointOffset = endPoints[i] * (offset / 100);
 
-      let startingPoint = this.__rotatePoint__(
+      let startingPoint = __rotatePoint__(
         [originX + i * increment, originY - pointOffset],
         [originX, originY],
         degree
       );
       rtn.start.push(startingPoint);
 
-      let endingPoint = this.__rotatePoint__(
+      let endingPoint = __rotatePoint__(
         [originX + i * increment, originY + endPoints[i] - pointOffset],
         [originX, originY],
         degree
@@ -185,140 +185,135 @@ export const getPoints = (
   }
 };
 
-export const drawCircle = ([x, y], diameter, options = {}) => {
-  let { color, lineColor = this.ctx.strokeStyle } = options;
+// export const drawCircle = ([x, y], diameter, options = {}) => {
+//   let { color, lineColor = ctx.strokeStyle } = options;
 
-  this.ctx.beginPath();
-  this.ctx.arc(x, y, diameter / 2, 0, 2 * Math.PI);
-  this.ctx.strokeStyle = lineColor;
-  this.ctx.stroke();
-  this.ctx.fillStyle = color;
-  if (color) this.ctx.fill();
-};
+//   ctx.beginPath();
+//   ctx.arc(x, y, diameter / 2, 0, 2 * Math.PI);
+//   ctx.strokeStyle = lineColor;
+//   ctx.stroke();
+//   ctx.fillStyle = color;
+//   if (color) ctx.fill();
+// };
 
-export const drawOval = ([x, y], height, width, options = {}) => {
-  let { rotation = 0, color, lineColor = this.ctx.strokeStyle } = options;
-  if (rotation) rotation = this.__toRadians__(rotation);
+// export const drawOval = ([x, y], height, width, options = {}) => {
+//   let { rotation = 0, color, lineColor = ctx.strokeStyle } = options;
+//   if (rotation) rotation = __toRadians__(rotation);
 
-  this.ctx.beginPath();
-  this.ctx.ellipse(x, y, width, height, rotation, 0, 2 * Math.PI);
-  this.ctx.strokeStyle = lineColor;
-  this.ctx.stroke();
-  this.ctx.fillStyle = color;
-  if (color) this.ctx.fill();
-};
+//   ctx.beginPath();
+//   ctx.ellipse(x, y, width, height, rotation, 0, 2 * Math.PI);
+//   ctx.strokeStyle = lineColor;
+//   ctx.stroke();
+//   ctx.fillStyle = color;
+//   if (color) ctx.fill();
+// };
 
-export const drawSquare = ([x, y], diameter, options = {}) => {
-  this.drawRectangle([x, y], diameter, diameter, options);
-};
+// export const drawSquare = ([x, y], diameter, options = {}) => {
+//   drawRectangle([x, y], diameter, diameter, options);
+// };
 
-export const drawRectangle = ([x, y], height, width, options = {}) => {
-  let {
-    color,
-    lineColor = this.ctx.strokeStyle,
-    radius = 0,
-    rotate = 0,
-  } = options;
+// export const drawRectangle = ([x, y], height, width, options = {}) => {
+//   let { color, lineColor = ctx.strokeStyle, radius = 0, rotate = 0 } = options;
 
-  // if (width < 2 * radius) radius = width / 2;
-  // if (height < 2 * radius) radius = height / 2;
+//   // if (width < 2 * radius) radius = width / 2;
+//   // if (height < 2 * radius) radius = height / 2;
 
-  this.ctx.beginPath();
-  this.ctx.moveTo(x + radius, y);
-  let p1 = this.__rotatePoint__([x + width, y], [x, y], rotate);
-  let p2 = this.__rotatePoint__([x + width, y + height], [x, y], rotate);
-  this.ctx.arcTo(p1[0], p1[1], p2[0], p2[1], radius);
+//   ctx.beginPath();
+//   ctx.moveTo(x + radius, y);
+//   let p1 = __rotatePoint__([x + width, y], [x, y], rotate);
+//   let p2 = __rotatePoint__([x + width, y + height], [x, y], rotate);
+//   ctx.arcTo(p1[0], p1[1], p2[0], p2[1], radius);
 
-  let p3 = this.__rotatePoint__([x + width, y + height], [x, y], rotate);
-  let p4 = this.__rotatePoint__([x, y + height], [x, y], rotate);
-  this.ctx.arcTo(p3[0], p3[1], p4[0], p4[1], radius);
+//   let p3 = __rotatePoint__([x + width, y + height], [x, y], rotate);
+//   let p4 = __rotatePoint__([x, y + height], [x, y], rotate);
+//   ctx.arcTo(p3[0], p3[1], p4[0], p4[1], radius);
 
-  let p5 = this.__rotatePoint__([x, y + height], [x, y], rotate);
-  let p6 = this.__rotatePoint__([x, y], [x, y], rotate);
-  this.ctx.arcTo(p5[0], p5[1], p6[0], p6[1], radius);
+//   let p5 = __rotatePoint__([x, y + height], [x, y], rotate);
+//   let p6 = __rotatePoint__([x, y], [x, y], rotate);
+//   ctx.arcTo(p5[0], p5[1], p6[0], p6[1], radius);
 
-  let p7 = this.__rotatePoint__([x, y], [x, y], rotate);
-  let p8 = this.__rotatePoint__([x + width, y], [x, y], rotate);
-  this.ctx.arcTo(p7[0], p7[1], p8[0], p8[1], radius);
-  this.ctx.closePath();
+//   let p7 = __rotatePoint__([x, y], [x, y], rotate);
+//   let p8 = __rotatePoint__([x + width, y], [x, y], rotate);
+//   ctx.arcTo(p7[0], p7[1], p8[0], p8[1], radius);
+//   ctx.closePath();
 
-  this.ctx.strokeStyle = lineColor;
-  this.ctx.stroke();
-  this.ctx.fillStyle = color;
-  if (color) this.ctx.fill();
-};
+//   ctx.strokeStyle = lineColor;
+//   ctx.stroke();
+//   ctx.fillStyle = color;
+//   if (color) ctx.fill();
+// };
 
-export const drawLine = ([fromX, fromY], [toX, toY], options = {}) => {
-  let { lineColor = this.ctx.strokeStyle } = options;
+// export const drawLine = ([fromX, fromY], [toX, toY], options = {}) => {
+//   let { lineColor = ctx.strokeStyle } = options;
 
-  this.ctx.beginPath();
-  this.ctx.moveTo(fromX, fromY);
-  this.ctx.lineTo(toX, toY);
-  this.ctx.strokeStyle = lineColor;
-  this.ctx.stroke();
-};
+//   ctx.beginPath();
+//   ctx.moveTo(fromX, fromY);
+//   ctx.lineTo(toX, toY);
+//   ctx.strokeStyle = lineColor;
+//   ctx.stroke();
+// };
 
-export const drawPolygon = (points, options = {}) => {
-  let {
-    color,
-    lineColor = this.ctx.strokeStyle,
-    radius = 0,
-    close = false,
-  } = options;
+// export const drawPolygon = (points, options = {}) => {
+//   let {
+//     color,
+//     lineColor = ctx.strokeStyle,
+//     radius = 0,
+//     close = false,
+//   } = options;
 
-  function getRoundedPoint(x1, y1, x2, y2, radius, first) {
-    let total = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-    let idx = first ? radius / total : (total - radius) / total;
+//   function getRoundedPoint(x1, y1, x2, y2, radius, first) {
+//     let total = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+//     let idx = first ? radius / total : (total - radius) / total;
 
-    return [x1 + idx * (x2 - x1), y1 + idx * (y2 - y1)];
-  }
+//     return [x1 + idx * (x2 - x1), y1 + idx * (y2 - y1)];
+//   }
 
-  function getRoundedPoints(pts, radius) {
-    let len = pts.length;
-    let res = new Array(len);
+//   function getRoundedPoints(pts, radius) {
+//     let len = pts.length;
+//     let res = new Array(len);
 
-    for (let i2 = 0; i2 < len; i2++) {
-      let i1 = i2 - 1;
-      let i3 = i2 + 1;
+//     for (let i2 = 0; i2 < len; i2++) {
+//       let i1 = i2 - 1;
+//       let i3 = i2 + 1;
 
-      if (i1 < 0) i1 = len - 1;
-      if (i3 == len) i3 = 0;
+//       if (i1 < 0) i1 = len - 1;
+//       if (i3 == len) i3 = 0;
 
-      let p1 = pts[i1];
-      let p2 = pts[i2];
-      let p3 = pts[i3];
+//       let p1 = pts[i1];
+//       let p2 = pts[i2];
+//       let p3 = pts[i3];
 
-      let prevPt = getRoundedPoint(p1[0], p1[1], p2[0], p2[1], radius, false);
-      let nextPt = getRoundedPoint(p2[0], p2[1], p3[0], p3[1], radius, true);
-      res[i2] = [prevPt[0], prevPt[1], p2[0], p2[1], nextPt[0], nextPt[1]];
-    }
-    return res;
-  }
+//       let prevPt = getRoundedPoint(p1[0], p1[1], p2[0], p2[1], radius, false);
+//       let nextPt = getRoundedPoint(p2[0], p2[1], p3[0], p3[1], radius, true);
+//       res[i2] = [prevPt[0], prevPt[1], p2[0], p2[1], nextPt[0], nextPt[1]];
+//     }
+//     return res;
+//   }
 
-  if (radius > 0) {
-    points = getRoundedPoints(points, radius);
-  }
+//   if (radius > 0) {
+//     points = getRoundedPoints(points, radius);
+//   }
 
-  let i,
-    pt,
-    len = points.length;
-  for (i = 0; i < len; i++) {
-    pt = points[i];
-    if (i == 0) {
-      this.ctx.beginPath();
-      this.ctx.moveTo(pt[0], pt[1]);
-    } else {
-      this.ctx.lineTo(pt[0], pt[1]);
-    }
-    if (radius > 0) {
-      this.ctx.quadraticCurveTo(pt[2], pt[3], pt[4], pt[5]);
-    }
-  }
+//   let i,
+//     pt,
+//     len = points.length;
+//   for (i = 0; i < len; i++) {
+//     pt = points[i];
+//     if (i == 0) {
+//       ctx.beginPath();
+//       ctx.moveTo(pt[0], pt[1]);
+//     } else {
+//       ctx.lineTo(pt[0], pt[1]);
+//     }
+//     if (radius > 0) {
+//       ctx.quadraticCurveTo(pt[2], pt[3], pt[4], pt[5]);
+//     }
+//   }
 
-  if (close) this.ctx.closePath();
-  this.ctx.strokeStyle = lineColor;
-  this.ctx.stroke();
+//   if (close) ctx.closePath();
+//   ctx.strokeStyle = lineColor;
+//   ctx.stroke();
 
-  this.ctx.fillStyle = color;
-  if (color) this.ctx.fill();
-};
+//   ctx.fillStyle = color;
+//   if (color) ctx.fill();
+// };
