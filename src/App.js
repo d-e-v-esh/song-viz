a303d497e4441c127cc616fabe0fd3852e37c9c7
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2021-02-20 18:28:29 +0530

    📊 Added Bounce And Root Line Features

--- src/App.js
+++ src/App.js
@@ -12,7 +12,14 @@ function App() {
       {/* <Visualizer /> */}
       {/* <CircleFunc /> */}
       {/* <SpaceForce /> */}
-      <NewSpaceForce />
+      <NewSpaceForce
+        bars={60}
+        bar_width={10}
+        fftSizeValue={2048}
+        barColor={"blue"}
+        RMSMultiplier={0.5}
+        baseRadiusValue={100}
+      />
       {/* <Canvas /> */}
     </div>
   );
--- src/components/SpaceForceNew.jsx
+++ src/components/SpaceForceNew.jsx
@@ -5,20 +5,28 @@ import rms from "../utils/RMS";
 // Changing Variables
 let ctx, rafId;
 
-// constants
+// TODO: need to change this
 const width = window.innerWidth;
 const height = window.innerHeight;
-const bars = 512;
-const bar_width = 2;
+// const barColor = "red";
+// const bar_width = 2;
+const bounce = true;
+const rootLineVisible = true;
 
-const NewSpaceForce = () => {
+const NewSpaceForce = ({
+  bars,
+  bar_width,
+  fftSizeValue,
+  barColor,
+  baseRadiusValue,
+  RMSMultiplier,
+}) => {
   const audio = new Audio(songFile);
   const context = new (window.AudioContext || window.webkitAudioContext)();
   const canvas = createRef();
   const analyser = context.createAnalyser();
-  analyser.fftSize = 2048; // This is the default anyway
+  analyser.fftSize = fftSizeValue; // This is the default anyway
   const bufferLength = analyser.frequencyBinCount;
-  console.log(bufferLength);
   const frequency_array = new Uint8Array(bufferLength);
 
   useEffect(() => {
@@ -28,28 +36,36 @@ const NewSpaceForce = () => {
     analyser.connect(context.destination);
   }, []);
 
-  const dataArray = new Uint8Array(analyser.fftSize); // Uint8Array should be the same length as the fftSize
-
   const animationLooper = (canvas) => {
+    ctx = canvas.getContext("2d");
     analyser.getByteFrequencyData(frequency_array);
     // console.log(frequency_array);
     canvas.width = width;
-
-    const currentRMS = rms(frequency_array);
-    const workingRMS = Math.max(100, 100 + currentRMS);
-    // console.log(workingRMS, currentRMS);
-
     canvas.height = height;
-    const baseRadius = workingRMS;
-    // const radius = baseRadius * loudness;
-    const radius = baseRadius;
 
-    ctx = canvas.getContext("2d");
+    let radius;
+    if (bounce) {
+      const currentRMS = rms(frequency_array);
+      const workingRMS = Math.max(
+        baseRadiusValue,
+        baseRadiusValue + currentRMS * RMSMultiplier
+      );
 
-    // ctx.beginPath();
-    // ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
-    // ctx.stroke();
-
+      var baseRadius = workingRMS;
+    } else if (!bounce) {
+      baseRadius = baseRadiusValue;
+    }
+    radius = baseRadius;
+    // console.log(workingRMS, currentRMS);
+    const makeRootLineVisible = (ctx) => {
+      ctx.beginPath();
+      ctx.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+      ctx.stroke();
+    };
+    if (rootLineVisible) {
+      makeRootLineVisible(ctx);
+      // console.log(ctx);
+    }
     // The average value here is just used to make the color of the bars and text react to the music
     const avg =
       [...Array(255).keys()].reduce(
@@ -70,7 +86,7 @@ const NewSpaceForce = () => {
       let y_end =
         canvas.height / 2 + Math.sin(radians * i) * (radius + bar_height);
 
-      let color = "#FF69B4";
+      let color = barColor;
       ctx.strokeStyle = color;
       ctx.lineWidth = bar_width;
       ctx.beginPath();
