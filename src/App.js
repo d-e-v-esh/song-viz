5a3d859867a89312387b9fa3e5b11c514ab5ffd3
d-e-v-esh <59534570+d-e-v-esh@users.noreply.github.com>
2021-04-04 03:48:29 +0530

    Button Outside Working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import NewSpaceForce from "./components/SpaceForceNew";
-import songFile from "./audio/water.wav";
-import kshmr from "./img/kshmrOneMoreRound.jpg";
+import React, {useState} from "react";
+// import NewSpaceForce from "./components/SpaceForceNew";
+import BarsCirc from './components/Bars'
+import songFile from "./audio/water.wav"; import kshmr from "./img/kshmrOneMoreRound.jpg";
 function App() {
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false)
+  const audio = new Audio(songFile); // Loading audio file
+  const majorToggle = () => {
+
+    if(audio.paused){
+      audio.play()
+    }else{
+      audio.pause()
+    }
+
+    // console.log(isAudioPlaying)
+  }
+
+
   return (
     <div>
-      <NewSpaceForce
+      <BarsCirc
         bars={600} // done
         barColor={"lightpink"} //  done
         baseRadiusValue={100} //  done
@@ -13,8 +27,11 @@ function App() {
         bounceMultiplier={1}
         centerImageSrc={kshmr} //  done
         circProperties={[12, "black"]}
+        isAudioPlaying={isAudioPlaying}
         audioSrc={songFile}
+        audio={audio}
       />
+      <button onClick={majorToggle}>Toggle</button>
     </div>
   );
 }
