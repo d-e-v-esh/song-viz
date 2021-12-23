5f337ec5dc6ace7971bc802f6374f27a91ced914
Devesh <deves125@gmail.com>
2021-12-23 20:57:22 +0530

    Added Media

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,40 @@
-import React from 'react'
-
-import { ExampleComponent } from 'audio-viz'
-import 'audio-viz/dist/index.css'
+import React, { useRef } from 'react'
+import songFile from './water.wav'
+import kshmr from './img/kshmrOneMoreRound.jpg'
+import { Viz } from 'audio-viz'
 
 const App = () => {
-  return <ExampleComponent text='Create React Library Exaasdfmple 😄' />
+  const audioRef = useRef()
+  return (
+    <div>
+      <Viz
+        canvasWidth={1000}
+        canvasHeight={1000}
+        audioRef={audioRef}
+        centerImageSrc={kshmr}
+        circleProps={{ circleWidth: 12, circleColor: 'black' }}
+        barColor={{
+          // colorTwo: "rgba(255,97,45,255)",
+          // colorOne: "rgba(253,235,184,255)",
+
+          hslColor: [2, 100, 50]
+        }}
+        radius={200}
+        rotation={true}
+        baseRadiusValue={100}
+        bounceMultiplier={0.5}
+        fftSizeValue={2048}
+        smoothingTimeConstant={0.8}
+        bars={200}
+        barWidth={4}
+        barHeightMultiplier={1}
+        centerColor='red'
+        canvasBackground='white'
+      />
+
+      <audio src={songFile} controls ref={audioRef} />
+    </div>
+  )
 }
 
 export default App
