c749c2a576a4ac65063affa73992fcb65fc542ea
Devesh <deves125@gmail.com>
2021-12-23 19:49:30 +0530

    Test

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'audio-viz'
 import 'audio-viz/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library Exaasdfmple 😄' />
 }
 
 export default App
